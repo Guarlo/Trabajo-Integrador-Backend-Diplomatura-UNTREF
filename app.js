f00b10542b2d94d0fddad286f4f0fdae6c0adaef
Guarlo <xguarlo@gmail.com>
2024-07-29 01:22:13 -0300

    Casi listo!

--- app.js
+++ app.js
@@ -20,50 +20,76 @@ app.get('/', (req, res) => {
 })
 
 //Mostrar un producto por codigo
-app.get('/productos/:codigo', (req, res) => {
-  console.log('/productos/:codigo')
-  const query = {}
-  const { codigo } = req.params
-  console.log(codigo)
-  console.log(esNumerico(codigo))
-  const validatedCode = esNumerico(codigo) ? parseInt(codigo, 10) : null
-
-  if (validatedCode === null) {
-    console.error('El código ingresado está mal formado!')
+app.get('/productos/:id', (req, res) => {
+  console.log('/productos/:id')
+  const { id } = req.params
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    console.error('El Id ingresado está mal formado!!!!')
     return res.status(404).json({
-      error: '404',
-      message: 'El código ingresado está mal formado!'
+      message: 'El Id ingresado está mal formado!!!!',
+      error: { status: 404 }
     })
   } else {
-    const query = { codigo: validatedCode }
-    console.log(query)
-    Collection.find(query)
+    console.log("El ID es válido")
+    console.log(id)
+    Collection.findById(id)
       .then((document) => {
-        if (Array.isArray(document) && document.length > 0) {
-          //console.log('Producto encontrado:', document)
+        if (document) {
+          if (Object.values(document).length > 0) {
+            console.log(Object.values(document).length)
+          }
           res.status(200).json(document)
         } else {
-          console.error('El código ingresado no existe!')
+          console.error('El Id ingresado no existe!')
           return res.status(404).json({
-            error: '404',
-            message: 'El código ingresado no existe!'
+            message: 'El Id ingresado no existe!',
+            error: { status: 404 }
           })
         }
       })
       .catch((error) => {
         console.error('Error al obtener el producto! ', error)
         return res.status(500).json({
-          error: '500',
-          message: 'Error al obtener el producto'
+          message: 'Error al obtener el producto',
+          error: { status: 500 }
         })
       })
   }
 })
 
-// Obtener todos los productos y opcionalmente de una categoría.
+// Obtener todos los productos y opcionalmente de una query de categoría y/o nombre.
 app.get('/productos', (req, res) => {
   const { categoria } = req.query
-  const query = !categoria ? {} : { categoria: { $regex: categoria, $options: 'i' } }
+  const { nombre } = req.query
+  const { codigo } = req.query
+
+  let errorMessage = ""
+  let query = {}
+
+  if (codigo) {
+    const validatedCode = isNumeric(codigo) ? parseInt(codigo, 10) : -1
+    //console.log(validatedCode.toString().length)
+    if (validatedCode === -1 || validatedCode.toString().length > 4) {
+      console.error('El código ingresado está mal formado!')
+      return res.status(404).json({
+        message: 'El código ingresado está mal formado!',
+        error: { status: 404 }
+      })
+    } else {
+      query = { codigo: validatedCode }
+    }
+  } else if (categoria && nombre) {
+    query = {
+      $and: [
+        { categoria: { $regex: new RegExp(createAccentInsensitiveRegex(categoria), "i") } },
+        { nombre: { $regex: new RegExp(createAccentInsensitiveRegex(nombre), "i") } }
+      ]
+    }
+  } else if (categoria) {
+    query = { categoria: { $regex: new RegExp(createAccentInsensitiveRegex(categoria), "i") } }
+  } else if (nombre) {
+    query = { nombre: { $regex: new RegExp(createAccentInsensitiveRegex(nombre), "i") } }
+  }
   console.log(query)
   Collection.find(query)
     .then((documents) => {
@@ -71,17 +97,127 @@ app.get('/productos', (req, res) => {
         res.status(200).json(documents)
       } else {
         console.error('No se encontraron productos!')
+        console.log(documents)
         return res.status(404).json({
-          error: '404',
-          message: 'No se encontraron productos!'
+          message: 'No se encontraron productos!',
+          error: { status: 404 }
         })
       }
     })
     .catch((error) => {
       console.error('Error al obtener los productos: ', error)
-      res.status(500).json({
-        error: '500',
-        message: 'Error al obtener los productos'
+      return res.status(500).json({
+        message: 'Error al obtener los productos!',
+        error: { status: 500 }
+      })
+    })
+})
+
+
+// Ruta para manejar el POST y agregar un Producto.
+app.post('/productos', validateRequestBody, (req, res) => {
+  const newProduct = new Collection(req.body)
+  console.log(newProduct)
+  generateUniqueCodigo()
+    .then(codigoUnico => {
+      newProduct.codigo = codigoUnico
+      console.log(newProduct)
+      return newProduct.save()
+    })
+    .then(savedProduct => {
+      //Created
+      res.status(201).json({ mensaje: "Documento insertado correctamente", datos: savedProduct })
+    })
+    .catch((error) => {
+      console.error('Error al agregar el producto! ', error)
+      return res.status(500).json({
+        message: 'Error al agregar el producto!',
+        error: {
+          status: 500,
+          description: error
+        }
+      })
+    })
+})
+
+//Borrar un producto
+app.delete('/productos/:id', (req, res) => {
+  const { id } = req.params
+  Collection.findByIdAndDelete(id)
+    .then((result) => {
+      if (result) {
+        console.log('Producto borrado con exito!')
+        console.log(result)
+        //Si el documento se elimina correctamente, devuelve un 204 No Content.
+        // , datos: result } no se envia si el status es 404
+        res.status(204).json({ message: 'Producto borrado con exito' })
+      } else {
+        console.error('No se encontró el producto solicitado para ser borrado!')
+        return res.status(404).json({
+          message: 'No se encontró el producto solicitado para ser borrado!',
+          error: { status: 404 }
+        })
+      }
+    })
+    .catch((error) => {
+      console.error('Error al borrar el producto! ', error)
+      return res.status(500).json({
+        message: 'Error al borrar el producto!',
+        error: {
+          status: 500,
+          description: error
+        }
+      })
+    })
+})
+
+//Modificar/Actualizar un producto parcialmente.
+app.patch('/productos/:id', (req, res) => {
+  const { id } = req.params
+  //const { precio } = req.body
+  const body = req.body
+
+  // Validar que el cuerpo de la solicitud contiene datos
+  if (!body || Object.keys(body).length === 0) {
+    return res.status(400).json({
+      message: 'El cuerpo de la solicitud no está correctamente instanciado',
+      error: {
+        status: 400
+      }
+    })    
+  }
+  if (!body.precio) {
+    return res.status(400).json({
+      message: 'El cuerpo de la solicitud no informa el precio a modificar',
+      error: {
+        status: 400
+      }
+    })    
+  }
+  const updateData = { precio: body.precio }
+  console.log(updateData)
+  Collection.findByIdAndUpdate(id, updateData, { new: true, overwrite: true, runValidators: true })
+    .then((result) => {
+      if (result) {
+        console.log('Producto actualizado con exito!')
+        console.log(result)
+        //Si el documento se modifica correctamente, devuelve un 204 No Content.
+        // , datos: result } no se envia si el status es 404
+
+        res.status(204).json({ message: 'Producto actualizado con exito!' })
+      } else {
+        console.log('No se pudo actualizar el producto!')
+        res.status(404).json({ message: 'No se pudo actualizar el producto!' })
+      }
+    })
+    .catch((error) => {
+      console.error('Error al modificar el producto! ', error)
+      return res.status(500).json({
+        message: 'Error al modificar el producto!',
+        error: {
+          status: 500,
+          description: error
+        }
       })
     })
 })
@@ -89,31 +225,123 @@ app.get('/productos', (req, res) => {
 
 // Middleware para manejar rutas inválidas
 app.use((req, res, next) => {
-
-  //throw new Error('Ruta no encontrada')
-  const err = new Error('Ruta no encontrada');
+  const err = new Error('Ruta no encontrada')
   err.status = 404
   next(err)
 })
 
 // Middleware de manejo de errores
 app.use((err, req, res, next) => {
-  //console.error(err.stack);
+  //console.error(err.stack) 
   res.status(err.status || 500).json({
     message: err.message,
     error: process.env.NODE_ENV === 'development' ? err : {},
-
   })
   //error: process.env.NODE_ENV === 'production' ? {} : err,
 })
-
 
 //Inicializamos el servidor
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
 })
 
-function esNumerico(valor) {
-  //return typeof valor === 'number' && !isNaN(valor);
-  return !isNaN(valor);
+
+
+//***************************************************************/
+// Funciones
+//***************************************************************/
+
+function isNumeric(valor) {
+  //return typeof valor === 'number' && !isNaN(valor) 
+  return !isNaN(valor)
+}
+
+// Función para construir la expresión regular que ignore acentos
+function createAccentInsensitiveRegex(str) {
+  const accentMap = {
+    a: 'aàáâãäå',
+    e: 'eèéêë',
+    i: 'iìíîï',
+    o: 'oòóôõö',
+    u: 'uùúûü',
+    A: 'aàáâãäå',
+    E: 'eèéêë',
+    I: 'iìíîï',
+    O: 'oòóôõö',
+    U: 'uùúûü',
+    c: 'cç',
+    n: 'nñ'
+  }
+  let pattern = ''
+  for (let char of str) {
+    if (accentMap[char]) {
+      pattern += `[${accentMap[char]}]`
+    } else {
+      pattern += char
+    }
+  }
+  return new RegExp(pattern, 'i')
+}
+
+
+// Función para generar un código único
+function generateUniqueCodigo() {
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * 9000) + 1000
+  }
+  function checkCodigo(codigo) {
+    return Collection.findOne({ codigo: codigo })
+      .then(result => {
+        if (!result) {
+          return codigo
+        }
+        // Si el código ya existe, generar uno nuevo y verificar nuevamente
+        return checkCodigo(generateRandomNumber())
+      })
+  }
+  // Iniciar con un nuevo código y verificar
+  return checkCodigo(generateRandomNumber())
+}
+
+
+// Función de validación
+function validateRequestBody(req, res, next) {
+  const ObjetoReqBody = req.body
+  let body = {}
+
+  if (ObjetoReqBody["codigo"]) {
+    //console.log(ObjetoReqBody["codigo"] + " Vino este ")
+    body = ObjetoReqBody
+  }
+  else {
+    //console.log(ObjetoReqBody["codigo"] + "  No vino")
+    const nuevaPropiedad = { codigo: 0 }
+    // Crear un nuevo objeto y agregar la nueva propiedad primero
+    body = { ...nuevaPropiedad, ...ObjetoReqBody }
+  }
+  console.log(body)
+
+  // Definición de las validaciones
+  const validations = {
+    codigo: value => typeof value === 'number' && value > -1,
+    nombre: value => typeof value === 'string' && value.trim().length > 0,
+    precio: value => typeof value === 'number' && value > 0,
+    categoria: value => typeof value === 'string' && value.trim().length > 0
+  }
+  // Recorrer y validar cada campo
+  for (const [key, validate] of Object.entries(validations)) {
+    if (!validate(body[key])) {
+      return res.status(400).json({
+        message: `Campo inválido: ${key}`,
+        error: {
+          status: 400,
+          description: `Campo inválido:`,
+          field: `${key}`
+        }
+      })
+    }
+  }
+  // Si todas las validaciones pasan, continuar con la siguiente función de middleware
+  req.body = body
+  next()
 }
